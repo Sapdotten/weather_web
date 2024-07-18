d4f3b5df4a5f45749c5acb68caa4560f1efc2060
Sapdotten <semenova.a.p.04@gmail.com>
2024-07-18 15:44:46 +0400

    Add class for worki with cities

---
+++ app/modules/city.py
@@ -0,0 +1,35 @@
+import logging
+from dadata import Dadata
+from utils.settings import Settings
+import requests
+
+
+class CityHelper:
+    city_api = Dadata(Settings.get_city_api())
+    geo_api = Settings.get_geocoder_api()
+    GEO_URL = 'https://geocode-maps.yandex.ru/1.x/'
+
+    @classmethod
+    def get_city_coords(cls, city: str) -> tuple:
+        """Retruns coordinates of place
+
+        Args:
+            city (str): name of place or city
+
+        Returns:
+            tuple: 
+        """
+        response = requests.get(cls.GEO_URL, params={
+                                'apikey': cls.geo_api, 'geocode': city, 'format': 'json', 'results': 1}).json()
+        return [float(num) for num in response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split(' ')]
+
+    @classmethod
+    def get_list_of_cities(cls, name: str) -> list[str]:
+        try:
+            return [city['value'] for city in cls.city_api.suggest("address", name)]
+        except Exception as e:
+            logging.warning(f"Can't find city by query {name}, error: {e}")
+            return []
+
+
+print(CityHelper.get_city_coords('Бузулук'))
